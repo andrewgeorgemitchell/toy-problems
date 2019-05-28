017c518e793e10e6078f022d2f338c909499b6d3
Andrew Mitchell <andrew.mitchell@adeptdc.com>
2019-05-28 08:35:37 -0700

    Add problem def & comments to reverseInteger.js

--- leetcode-reverseInteger/leetcode-reverseInteger.js
+++ leetcode-reverseInteger/leetcode-reverseInteger.js
@@ -1,7 +1,30 @@
-/**
- * @param {number} x
- * @return {number}
- */
+//
+// Problem Definition:
+//
+
+/*
+
+Given a 32-bit signed integer, reverse digits of an integer.
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers
+within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem,
+assume that your function returns 0 when the reversed integer overflows.
+
+*/
+
 const reverse = (x) => {
   if (x === 0) {
     return x;
@@ -22,4 +45,7 @@ const reverse = (x) => {
   return parseInt(stringifiedDigits, 10);
 };
 
+//
+// Tests
+//
 console.log(reverse(123) === 321);
