b3e20e0554a01a0a42e791333b1ded5568f085be
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2020-02-09 21:21:09 -0800

    Update islandCount.js

--- islandCount/islandCount.js
+++ islandCount/islandCount.js
@@ -3,6 +3,12 @@
  * Land spaces are denoted by a zero, while water is denoted by a dot. Two land spaces
  * are considered connected if they are adjacent (but not diagonal).
  *
+ * 0, 0, 0, 1, 1,
+ * 0, 0, 0, 1, 1,
+ * 0, 1, 1, 0, 0,
+ * 0, 0, 0, 0, 0,
+ * 1, 1, 0, 0, 0,
+ *
  */
 
 function countIslands(mapStr) {
